7b5d5452f7e278170ab626cbf70cbc68b9516364
Ferran Gurri Mancera <ferran.gurri@gmail.com>
2019-05-09 13:00:31 +0200

    Dashboard added

--- nautilus/urls.py
+++ nautilus/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('dashboard', views.dashboard, name='dashboard'),
     path('pm', views.plotpm, name='pm'),
     path('pepm', views.plotpepm, name='pepm'),
     path('pcpe', views.plotpcpe, name='pcpe')
--- nautilus/views.py
+++ nautilus/views.py
@@ -2,13 +2,27 @@ import nautilus.plots as p
 from django.shortcuts import render
 
 
+def dashboard(request):
+    return render(request, 'dashboard.html', {'v_visits_total' : 1580502,
+        'v_visits_current_year': 73642,
+        'v_visits_last_year': 67059,
+        'v_patients_total': 100879,
+        'v_patients_current_year': 18757,
+        'v_patients_last_year': 17396,
+        'v_new_patients_current_year': 2227,
+        'v_new_patients_last_year': 2360,
+        'v_fidelity_total': 15.67,
+        'v_fidelity_current_year': 3.93,
+        'v_fidelity_last_year': 3.85,})
+
+
 def plotpm(request):
-    return render(request, 'base.html', {'plotdiv': p.generate_pm()})
+    return render(request, 'plot.html', {'plotdiv': p.generate_pm()})
 
 
 def plotpepm(request):
-    return render(request, 'base.html', {'plotdiv': p.generate_pepm(13)})
+    return render(request, 'plot.html', {'plotdiv': p.generate_pepm(13)})
 
 
 def plotpcpe(request):
-    return render(request, 'base.html', {'plotdiv': p.generate_pcpe()})
+    return render(request, 'plot.html', {'plotdiv': p.generate_pcpe()})
